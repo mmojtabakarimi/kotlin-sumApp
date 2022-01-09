120916a1f28a63e86b8248aa49bb806ee277493c
karimi <karimi@rayocom.ir>
2022-01-09 15:01:51 +0330

    Add some possible Input  for  test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,31 @@
 fun main() {
-    val inputDigitString: String = "five-One-two-Nine-Three-Zero-seven"
+    var inputDigitString: String = "five-One-two-Nine-Three-Zero-seven"
     val sumApp = SumClass()
 
-    println("The Result will be: ${sumApp.makeDigitStringToNumberString(inputDigitString)}${sumApp.sumInput(inputDigitString)}")
+    println(
+        "The Result will be: ${sumApp.makeDigitStringToNumberString(inputDigitString)}${
+            sumApp.sumInput(
+                inputDigitString
+            )
+        }"
+    )
+
+    inputDigitString = "Six-rr-five-One-two-Nine-Three-Zero-seven"
+    println(
+        "The Result will be: ${sumApp.makeDigitStringToNumberString(inputDigitString)}${
+            sumApp.sumInput(
+                inputDigitString
+            )
+        }"
+    )
+
+    inputDigitString = "One-Alll-Four-Three-Zero-seven-Six-1-2-five-Oneone-two@%%-Nine:-Three-Zero-seven"
+    println(
+        "The Result will be: ${sumApp.makeDigitStringToNumberString(inputDigitString)}${
+            sumApp.sumInput(
+                inputDigitString
+            )
+        }"
+    )
+
 }
