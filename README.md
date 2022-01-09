8fde56b347954698215019ab7926a38b8da42cbd
karimi <karimi@rayocom.ir>
2022-01-09 15:10:01 +0330

    Update  Readme and tests in main function

--- README.md
+++ README.md
@@ -2,4 +2,17 @@ Please create a small project in Kotlin that contains a function that takes an i
 and returns a string with the format "1+2+7+4+9+0=23".
 Then test the function with some possible inputs (but please not use the input from this description), 
 you can do this directly in the main function. It should be possible to pass the digits 0-9. 
-Everything that is not specified you can solve as you like it or as you think it makes sense
+Everything that is not specified you can solve as you like it or as you think it makes sense.
+
+Tests:
+Test 1)
+"five-One-two-Nine-Three-Zero-seven"
+5+1+2+9+3+0+7=27
+
+Test 2)
+"Six-five-One-two-Nine-Three-Zero-seven"
+6+5+1+2+9+3+0+7=33
+
+Test 3)
+"One-Alll-Four-Three-Zero-seven-Six-1-2-five-Oneone-two@%%-Nine:-Three-Zero-seven"
+1+4+3+0+7+6+5+3+0+7=36
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,27 +1,27 @@
 fun main() {
     var inputDigitString: String = "five-One-two-Nine-Three-Zero-seven"
     val sumApp = SumClass()
-
+    //Test normal digit string
     println(
-        "The Result will be: ${sumApp.makeDigitStringToNumberString(inputDigitString)}${
+        "${sumApp.makeDigitStringToNumberString(inputDigitString)}${
             sumApp.sumInput(
                 inputDigitString
             )
         }"
     )
-
-    inputDigitString = "Six-rr-five-One-two-Nine-Three-Zero-seven"
+    //Test string may contain case sensitive digit  string
+    inputDigitString = "Six-five-One-two-Nine-Three-Zero-seven"
     println(
-        "The Result will be: ${sumApp.makeDigitStringToNumberString(inputDigitString)}${
+        "${sumApp.makeDigitStringToNumberString(inputDigitString)}${
             sumApp.sumInput(
                 inputDigitString
             )
         }"
     )
-
+    //Test string may contain anything
     inputDigitString = "One-Alll-Four-Three-Zero-seven-Six-1-2-five-Oneone-two@%%-Nine:-Three-Zero-seven"
     println(
-        "The Result will be: ${sumApp.makeDigitStringToNumberString(inputDigitString)}${
+        "${sumApp.makeDigitStringToNumberString(inputDigitString)}${
             sumApp.sumInput(
                 inputDigitString
             )
